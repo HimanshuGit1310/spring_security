17e8c7100550aba99d382fa1db88365fa219f734
HimanshuGit1310 <himanshujhariyazeal@gmail.com>
2025-07-16 23:55:25 +0530

    JWT token Generate and implemented

---
+++ security/src/main/java/com/zeal/security/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.zeal.security.controller;
+
+import com.zeal.security.model.Users;
+import com.zeal.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user){
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Users user){
+        return userService.verify(user);
+    }
+}
---
+++ security/src/main/java/com/zeal/security/service/UserService.java
@@ -0,0 +1,38 @@
+package com.zeal.security.service;
+
+import com.zeal.security.model.Users;
+import com.zeal.security.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JWTService jwtService;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+        if (authentication.isAuthenticated())
+            return jwtService.genrateToken(user.getUsername());
+        return "fail";
+    }
+}
